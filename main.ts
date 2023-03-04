2dbe66d6d3a1a15c0670d6f8391ee42d9c9afc0d
Daniel-v8 <96905727+Daniel-v8@users.noreply.github.com>
2023-03-04 16:52:49 +0100

    Aktualizovať pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
     radio.sendNumber(8)
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(10)
+})
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
     radio.sendNumber(6)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(12)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(11)
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
     radio.sendNumber(7)
@@ -12,6 +21,7 @@ joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType
 })
 joystickbit.initJoystickBit()
 radio.setGroup(0)
+basic.showString("Ready to play!")
 basic.forever(function () {
     if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 250 && (joystickbit.getRockerValue(joystickbit.rockerType.X) > 400 && joystickbit.getRockerValue(joystickbit.rockerType.X) < 600)) {
         radio.sendNumber(1)
